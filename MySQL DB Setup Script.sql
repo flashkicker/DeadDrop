8e34a30ca7a51fef7bd6089511ebc88e2775db9f
Prasad Hanwadikar <hanwadip@oregonstate.edu>
2017-10-17 02:24:35 -0700

    DB setup script

---
+++ MySQL DB Setup Script.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE `deaddrop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `messages` (
+  `uuid` int(11) NOT NULL AUTO_INCREMENT,
+  `message` varchar(300) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `latitude` decimal(13,10) NOT NULL,
+  `longitude` decimal(13,10) NOT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
